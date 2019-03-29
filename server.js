95c073070a743b3ad8524bc5155a730691af53de
joakimolofsson <jjoakim.olofsson@gmail.com>
2019-03-29 18:01:18 +0100

    mongoose

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const api = require('./routes/api');
+const mongoose = require('mongoose');
 const path = require('path');
+const api = require('./routes/api');
+
+mongoose.connect('mongodb://joakimolofsson:clulosen123!@devcluster-shard-00-00-oioh2.gcp.mongodb.net:27017,devcluster-shard-00-01-oioh2.gcp.mongodb.net:27017,devcluster-shard-00-02-oioh2.gcp.mongodb.net:27017/mern_app_db?ssl=true&replicaSet=DevCluster-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true }, function(err) {
+    console.log(err);
+});
 
 const app = express();
 const port = process.env.PORT || 5000;
