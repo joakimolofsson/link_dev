8a1f2c79757bcae07afb0a006365a56842c26254
joakimolofsson <jjoakim.olofsson@gmail.com>
2019-03-29 16:04:37 +0100

    links

--- client/src/components/Links.js
+++ client/src/components/Links.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import './css/Links.css';
 
 class Links extends Component {
+    state = {
+        response: 'resp'
+    }
+
+    componentDidMount = () => {
+        this.fetchLinks();
+    }
+
+    fetchLinks = async () => {
+        try {
+            const getLinks = await fetch('/api/links', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    filter: 'hejsan links'
+                })
+            });
+            const res = await getLinks.json();
+            this.setState({
+                response: res.msg
+            });
+        } catch(err) {
+            console.log(err);
+            this.setState({serverMsg: 'Something went wrong!'});
+        }
+    }
+    
     render() {
         return(
             <div className="Links">
                 <h1>LINKS</h1>
+                <p>{this.state.response}</p>
             </div>
         )
     }
--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/hello', (req, res) => {
-    res.send({ express: 'Hello From Express' });
-});
-
-router.post('/world', (req, res) => {
-    console.log(req.body);
-    res.send(
-        `I received your POST request. This is what you sent me: ${req.body.post}`,
-    );
+router.post('/links', async(req, res) => {
+    try {
+        return res.json({
+            msg: req.body.filter
+        });
+    } catch(err) {
+        console.log(`Failed to get links | ${Date()} | ${err}`);
+        return res.json({msg: 'Something went wrong!'});
+    }
 });
 
 module.exports =  router;
